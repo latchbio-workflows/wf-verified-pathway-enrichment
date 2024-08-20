d8f9a81ebf5ea0c94cc5e17f3c9b046c93552c3b
Tahir D'Mello <tahirdme@gmail.com>
2024-08-19 17:40:39 -0700

    wiki links

--- wf/__init__.py
+++ wf/__init__.py
@@ -8,24 +8,31 @@ from wf.pathway_enrichment import Reference_Type, pathway_enrichment
 
 metadata = LatchMetadata(
     display_name="Pathway Enrichment",
+    documentation="https://wiki.latch.bio/workflows/bulk-rna-seq#bulk-rnaseq-quantification",
+    wiki_url="https://wiki.latch.bio/workflows/bulk-rna-seq#bulk-rnaseq-quantification",
     author=LatchAuthor(
         name="LatchBio",
+        github="https://github.com/latchbio-workflows/wf-verified-pathway-enrichment",
     ),
     parameters={
         "run_name": LatchParameter(
             display_name="Name of Run",
+            description="Name of run.",
             batch_table_column=True,
         ),
         "contrast_file": LatchParameter(
             display_name="Input Contrast File",
+            description="CSV file containing log-fold change, p-values, etc. from a comparison between conditions from DESeq2.",
             batch_table_column=True,
         ),
         "genome": LatchParameter(
-            display_name="Genome",
+            display_name="Reference Genome",
+            description="Reference genome for run.",
             batch_table_column=True,
         ),
         "output_directory": LatchParameter(
             display_name="Output Directory",
+            description="The output directory where the results will be saved.",
             batch_table_column=True,
         ),
     },
@@ -33,19 +40,36 @@ metadata = LatchMetadata(
 
 
 @workflow(metadata)
-def pathway_enrichment_wf(
+def gene_ontology_pathway_analysis(
     run_name: str,
     contrast_file: LatchFile,
     genome: Reference_Type,
     output_directory: LatchOutputDir = LatchOutputDir("latch:///Pathway_Enrichment"),
 ) -> LatchOutputDir:
-    """Pathway Enrichment
+    """Implicate pathways and gene ontologies from a sample contrast comparison of differential gene results
 
-    ## Pathway Enrichment
+    <html>
+    <p align="center">
+      <img src="https://user-images.githubusercontent.com/31255434/182289305-4cc620e3-86ae-480f-9b61-6ca83283caa5.jpg" alt="Latch Verified" width="100">
+    </p>
+
+    <p align="center">
+    <strong>
+    Latch Verified
+    </strong>
+    </p>
+
+    <p align="center">
+      Implicate pathways and gene ontologies from a set of desired genes.
+    </p>
+
+    </html>
 
     Under the hood, the workflow uses clusterprofiler for Gene Set Enrichment Analysis (GSEA) and Over-representation analysis (ORA) with mSigDB, KEGG, WikiPathway, Disease Ontology (DO) and Gene Ontology (GO) pathway databases.
+
     For more information, see our
     [wiki]("https://wiki.latch.bio/workflows/bulk-rna-seq#bulk-rnaseq-quantification").
+
     """
 
     return pathway_enrichment(
@@ -57,7 +81,7 @@ def pathway_enrichment_wf(
 
 
 LaunchPlan(
-    pathway_enrichment_wf,
+    gene_ontology_pathway_analysis,
     "Human Test Data",
     {
         "run_name": "human_pe_demo",
